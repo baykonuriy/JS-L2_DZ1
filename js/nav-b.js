de4cebb6fc402e04962bb003cc58a2ade0b8d386
Jura <baykonuriy@gmail.com>
2021-08-06 15:48:33 +0300

    add dz2

--- js/nav-b.js
+++ js/nav-b.js
@@ -1,141 +1,157 @@
-'use strict'
-
-document.querySelector('.nav__wrp__bar').insertAdjacentHTML(
-    'beforeend',
-    `<section class="nav-bacet">
-        <div class="nav-bacet__card nav-bacet__card_header">
-            <div class="nav-bacet__card__item nav-bacet__card__item_img-wrp">
-                <p>Product Name</p>
-            </div>
-            <div class="nav-bacet__card__item nav-bacet__card__item_amount">
-                <p>Amount</p>
-            </div>
-            <div class="nav-bacet__card__item nav-bacet__card__item_price">
-                <p>Price</p>
-            </div>
-            <div class="nav-bacet__card__item nav-bacet__card__item_total">
-                <p>Total price</p>
-            </div>
-        </div>
-        <div class="nav-bascet__cntnr">
-        </div>
-        <div class="nav-bacet__footer">
-            <div class="nav-bacet__footer__item nav-bacet__footer__item_header">
-                <p>Total purchase amount</p>
-            </div>
-            <div class="nav-bacet__footer__item nav-bacet__footer__item_amo">
-                <p>0</p>
-            </div>
-        </div>
-    </section>`);
-
-document.querySelector('.icon-button__counter').innerHTML = '0';
-
-let cardP = document.querySelectorAll('.card-product');
-let basketCard = document.querySelector('.nav-bascet__cntnr');
-
-cardP.forEach(function (elem) {
-    elem.addEventListener('click', function (e) {
-        if (e.target == e.currentTarget.querySelector('.newbtn_ghost_dark') ||
-            e.target == e.currentTarget.querySelector('.newbtn__txt') ||
-            e.target == e.currentTarget.querySelector('.newbtn_ghost_dark svg')) {
-            checkContainer(e.currentTarget);
-        }
-    })
-})
-
-function checkContainer(elem) {
-    const crdP = {
-        wrp: elem,
-        product: elem.querySelector('.card-product__description__h4'),
-        price: elem.querySelector('.card-product__price'),
-        img: elem.querySelector('.card-product__image')
+class NavBascetCard {
+    constructor(wrp, name, price, img) {
+        this.wrp = wrp;
+        this.name = name;
+        this.price = price;
+        this.img = img;
     }
-    if (basketCard.childElementCount == 0) {
-        cardMaker(crdP.product.textContent, crdP.price.textContent, crdP.img.getAttribute('src'));
-        console.log(crdP.product.textContent + ' ' + crdP.price.textContent + ' ' + crdP.img.getAttribute('src'));
-    }
-    else if (basketCard.childElementCount > 0) {
-        let crdsB = document.querySelectorAll('.nav-bacet__card');
-        for (let i = 0; i < crdsB.length; i++) {
-            const crdB = {
-                wrp: crdsB[i],
-                product: crdsB[i].querySelector('.nav-bacet__card__item_img-wrp p'),
-                amount: crdsB[i].querySelector('.nav-bacet__card__item_amount p'),
-                price: crdsB[i].querySelector('.nav-bacet__card__item_price p'),
-                total: crdsB[i].querySelector('.nav-bacet__card__item_total p')
-            }
-            if (crdB.product.textContent == crdP.product.textContent) {
-                let amount = Number(crdB.amount.textContent);
-                ++amount;
-                crdB.amount.innerHTML = String(amount);
-                let total = Number(crdB.total.textContent) + Number(crdB.price.textContent);
-                crdB.total.innerHTML = String(total);
-                return changeCount();
-            }
-            if (crdB.product.textContent !== crdP.product.textContent && i == crdsB.length - 1) {
-                cardMaker(crdP.product.textContent, crdP.price.textContent, crdP.img.getAttribute('src'));
-            }
-        }
-    }
-    changeCount();
-}
-
-function changeCount() {
-    let items = document.querySelectorAll('.nav-bascet__cntnr .nav-bacet__card');
-    let allTotal = 0;
-    let navCounter = 0;
-    items.forEach(function (elem) {
-        const counts = {
-            allTotal: document.querySelector('.nav-bacet__footer__item_amo p'),
-            navCounter: document.querySelector('.icon-button__counter'),
-        }
-        const countsNavCard = {
-            amount: elem.querySelector('.nav-bacet__card__item_amount p'),
-            total: elem.querySelector('.nav-bacet__card__item_total p')
-        }
-        allTotal = allTotal + Number(countsNavCard.total.textContent);
-        counts.allTotal.innerHTML = String(allTotal);
-        navCounter = navCounter + Number(countsNavCard.amount.textContent);
-        counts.navCounter.innerHTML = String(navCounter);
-    })
-}
-
-function cardMaker(name, price, img) {
-    basketCard.insertAdjacentHTML("afterbegin", `
+    render() {
+        return document.querySelector('.nav-bascet__cntnr').insertAdjacentHTML('afterbegin',
+            `
                 <div class="nav-bacet__card">
                     <div class="nav-bacet__card__item nav-bacet__card__item_img-wrp">
-                        <img src="${img}" alt="">
-                        <p>${name}</p>
+                        <img src="${this.img}" alt="">
+                        <p>${this.name}</p>
                     </div>
                     <div class="nav-bacet__card__item nav-bacet__card__item_amount">
                         <p>1</p>
                     </div>
                     <div class="nav-bacet__card__item nav-bacet__card__item_price">
-                        <p>${price}</p>
+                        <p>${this.price}</p>
                     </div>
                     <div class="nav-bacet__card__item nav-bacet__card__item_total">
-                        <p>${price}</p>
+                        <p>${this.price}</p>
                     </div>
                     <button class="nav-bacet__card__button">
-                     </button>
+                        </button>
                 </div>
-            `)
+            `);
+    }
 }
 
-basketCard.addEventListener('click', function (e) {
-    let cCard = e.currentTarget.querySelectorAll('.nav-bascet__cntnr .nav-bacet__card');
-    if (cCard.length > 0) {
-        cCard.forEach(function (elem) {
-            if (e.target == elem.querySelector('.nav-bacet__card__button') && cCard.length > 1) {
-                elem.remove();
-                changeCount();
+class NavBascet {
+    render() {
+        return document.querySelector('.nav__wrp__bar').insertAdjacentHTML(
+            'beforeend',
+            `<section class="nav-bacet">
+                <div class="nav-bacet__card nav-bacet__card_header">
+                    <div class="nav-bacet__card__item nav-bacet__card__item_img-wrp">
+                        <p>Product Name</p>
+                    </div>
+                    <div class="nav-bacet__card__item nav-bacet__card__item_amount">
+                        <p>Amount</p>
+                    </div>
+                    <div class="nav-bacet__card__item nav-bacet__card__item_price">
+                        <p>Price</p>
+                    </div>
+                    <div class="nav-bacet__card__item nav-bacet__card__item_total">
+                        <p>Total price</p>
+                    </div>
+                </div>
+                <div class="nav-bascet__cntnr">
+                </div>
+                <div class="nav-bacet__footer">
+                    <div class="nav-bacet__footer__item nav-bacet__footer__item_header">
+                        <p>Total purchase amount</p>
+                    </div>
+                    <div class="nav-bacet__footer__item nav-bacet__footer__item_amo">
+                        <p>0</p>
+                    </div>
+                </div>
+            </section>`);
+    }
+    checkContainer = elem => {
+        const productCard = {
+            wrp: elem,
+            name: elem.querySelector('.card-product__description__h4').textContent,
+            price: elem.querySelector('.card-product__price').textContent,
+            img: elem.querySelector('.card-product__image').getAttribute('src')
+        }
+        let container = document.querySelector('.nav-bascet__cntnr');
+        let navBascetCard = new NavBascetCard(productCard.wrp, productCard.name, productCard.price, productCard.img);
+        let bc = new NavBascet();
+        if (container.childElementCount == 0) {
+            navBascetCard.render();
+            bc.changeCount()
+        }
+        else if (container.childElementCount > 0) {
+            let bascetCardList = document.querySelectorAll('.nav-bacet__card');
+            for (let i = 0; i < bascetCardList.length; i++) {
+                const bascetCard = {
+                    wrp: bascetCardList[i],
+                    name: bascetCardList[i].querySelector('.nav-bacet__card__item_img-wrp p'),
+                    amount: bascetCardList[i].querySelector('.nav-bacet__card__item_amount p'),
+                    price: bascetCardList[i].querySelector('.nav-bacet__card__item_price p'),
+                    total: bascetCardList[i].querySelector('.nav-bacet__card__item_total p')
+                }
+                if (bascetCard.name.textContent == productCard.name) {
+                    let amount = Number(bascetCard.amount.textContent);
+                    ++amount;
+                    bascetCard.amount.innerHTML = String(amount);
+                    let total = Number(bascetCard.total.textContent) + Number(bascetCard.price.textContent);
+                    return bascetCard.total.innerHTML = String(total);
+                }
+                if (bascetCard.name.textContent !== productCard.name && i == bascetCardList.length - 1) {
+                    navBascetCard.render();
+                }
             }
-            if (e.target == elem.querySelector('.nav-bacet__card__button') && cCard.length == 1) {
-                elem.remove();
-                document.querySelector('.nav-bacet__footer__item_amo p').innerHTML = '0';
-                document.querySelector('.icon-button__counter').innerHTML = '0';
-            }
+
+        }
+    }
+    getNavBascetCard = () => {
+        let currentCard = document.querySelectorAll('.card-product');
+        currentCard.forEach(function (elem) {
+            elem.addEventListener('click', function (e) {
+                if (e.target == e.currentTarget.querySelector('.newbtn_ghost_dark') ||
+                    e.target == e.currentTarget.querySelector('.newbtn__txt') ||
+                    e.target == e.currentTarget.querySelector('.newbtn_ghost_dark svg')) {
+                    let bc = new NavBascet();
+                    bc.checkContainer(e.currentTarget);
+                    bc.changeCount();
+                }
+            })
         })
     }
-});
+    changeCount = () => {
+        let items = document.querySelectorAll('.nav-bascet__cntnr .nav-bacet__card');
+        let allTotal = 0;
+        let navCounter = 0;
+        items.forEach(function (elem) {
+            const counts = {
+                allTotal: document.querySelector('.nav-bacet__footer__item_amo p'),
+                navCounter: document.querySelector('.icon-button__counter'),
+            }
+            const countsNavCard = {
+                amount: elem.querySelector('.nav-bacet__card__item_amount p'),
+                total: elem.querySelector('.nav-bacet__card__item_total p')
+            }
+            allTotal = allTotal + Number(countsNavCard.total.textContent);
+            counts.allTotal.innerHTML = String(allTotal);
+            navCounter = navCounter + Number(countsNavCard.amount.textContent);
+            counts.navCounter.innerHTML = String(navCounter);
+        })
+    }
+    removeCard() {
+        document.querySelector('.nav-bascet__cntnr').addEventListener('click', function (e) {
+            let cCard = e.currentTarget.querySelectorAll('.nav-bascet__cntnr .nav-bacet__card');
+            if (cCard.length > 0) {
+                cCard.forEach(function (elem) {
+                    if (e.target == elem.querySelector('.nav-bacet__card__button') && cCard.length > 1) {
+                        elem.remove();
+                        changeCount();
+                    }
+                    if (e.target == elem.querySelector('.nav-bacet__card__button') && cCard.length == 1) {
+                        elem.remove();
+                        document.querySelector('.nav-bacet__footer__item_amo p').innerHTML = '0';
+                        document.querySelector('.icon-button__counter').innerHTML = '0';
+                    }
+                })
+            }
+        });
+    }
+}
+
+let navBascet = new NavBascet();
+navBascet.render();
+navBascet.getNavBascetCard();
+navBascet.removeCard();
--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,46 +1,74 @@
-const prodCard = [
-    {
-        title: 'Shirt',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        price: 150
-    },
-    {
-        title: 'Socks',
-        description: 'Dolor sit amet, consectetur adipiscing elit',
-        price: 350
-    },
-    {
-        title: 'Shoes',
-        description: 'Consectetur adipiscing elit. Dolor sit amet, consectetur adipiscing elit',
-        price: 250
-    },
-    {}
-];
-//добавил значения по умолчанию для аргументов функции
-const renderCardItem = (title = 'Futbolka', description = 'Lorem ipsum dolor sit amet', price = '150') => {
-    return `<article class="card-product">
-        <div class="card-product__wrapper">
-            <a href="#" class="card-product_hover">
-                <div class="newbtn_ghost_dark">
-                    <svg width="27" height="24" viewBox="0 0 27 24" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M21.4565 24C21.4088 24 21.3448 24 21.2963 24C20.1587 24 19.2137 23.0873 19.1335 21.9295C19.0535 20.7232 19.9507 19.6474 21.1523 19.5654C21.2012 19.5621 21.2499 19.5605 21.2982 19.5605C22.437 19.5605 23.3984 20.4624 23.4761 21.6196C23.5076 22.2233 23.3312 22.7936 22.9468 23.2342C22.5626 23.691 22.0334 23.9514 21.4565 24ZM7.67657 23.9936C6.47235 23.9936 5.49677 23.0036 5.49677 21.7809C5.49677 20.5592 6.47235 19.5691 7.67657 19.5691C8.88074 19.5691 9.85638 20.5592 9.85638 21.7809C9.85638 23.0036 8.88074 23.9936 7.67657 23.9936ZM20.6555 17.1202H8.59009C8.15717 17.1202 7.77289 16.8265 7.66034 16.4031L3.73456 1.95779H0.97876C0.450439 1.95779 0.0175781 1.51823 0.0175781 0.979444C0.0175781 0.441513 0.450439 0.000976562 0.97876 0.000976562H4.45636C4.88837 0.000976562 5.27264 0.294627 5.38513 0.719032L9.31097 15.1634H20.0146L23.5715 6.84915H11.7148C11.1857 6.84915 10.7527 6.40861 10.7527 5.8708C10.7527 5.33287 11.1857 4.89233 11.7148 4.89233H25.0457C25.3661 4.89233 25.6711 5.05535 25.8467 5.33287C26.0231 5.60941 26.0555 5.95182 25.9268 6.26172L21.5366 16.5328C21.3763 16.8914 21.0397 17.1202 20.6555 17.1202Z" />
-                    </svg>
-                    <span class="newbtn__txt">Add to&nbsp;Cart</span>
-                </div>
-            </a>
-            <img src="img/products/img1.png" alt="" class="card-product__image">
-        </div>
-        <div class="card-product__description">
-            <a href="#" class="card-product__description__h4">${title}</a>
-            <p class="card-product__description__p">${description}</p>
-            <span class="card-product__price">${price}</span>
-        </div>
-    </article> `
-};
-// сократил запись функци и убрал скобки для единсвенного агрумента
-const renderCardList = list => {
-    //добавил метод join чтобы изсчезла запистая создаваемая map
-    document.querySelector('.filter').insertAdjacentHTML('afterend', list.map(item => renderCardItem(item.title, item.description, item.price)).join(''));
+class ProductCardItem {
+    constructor(title, price, description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+    render() {
+        return `<article class="card-product">
+                    <div class="card-product__wrapper">
+                        <a href="#" class="card-product_hover">
+                            <div class="newbtn_ghost_dark">
+                                <svg width="27" height="24" viewBox="0 0 27 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M21.4565 24C21.4088 24 21.3448 24 21.2963 24C20.1587 24 19.2137 23.0873 19.1335 21.9295C19.0535 20.7232 19.9507 19.6474 21.1523 19.5654C21.2012 19.5621 21.2499 19.5605 21.2982 19.5605C22.437 19.5605 23.3984 20.4624 23.4761 21.6196C23.5076 22.2233 23.3312 22.7936 22.9468 23.2342C22.5626 23.691 22.0334 23.9514 21.4565 24ZM7.67657 23.9936C6.47235 23.9936 5.49677 23.0036 5.49677 21.7809C5.49677 20.5592 6.47235 19.5691 7.67657 19.5691C8.88074 19.5691 9.85638 20.5592 9.85638 21.7809C9.85638 23.0036 8.88074 23.9936 7.67657 23.9936ZM20.6555 17.1202H8.59009C8.15717 17.1202 7.77289 16.8265 7.66034 16.4031L3.73456 1.95779H0.97876C0.450439 1.95779 0.0175781 1.51823 0.0175781 0.979444C0.0175781 0.441513 0.450439 0.000976562 0.97876 0.000976562H4.45636C4.88837 0.000976562 5.27264 0.294627 5.38513 0.719032L9.31097 15.1634H20.0146L23.5715 6.84915H11.7148C11.1857 6.84915 10.7527 6.40861 10.7527 5.8708C10.7527 5.33287 11.1857 4.89233 11.7148 4.89233H25.0457C25.3661 4.89233 25.6711 5.05535 25.8467 5.33287C26.0231 5.60941 26.0555 5.95182 25.9268 6.26172L21.5366 16.5328C21.3763 16.8914 21.0397 17.1202 20.6555 17.1202Z" />
+                                </svg>
+                                <span class="newbtn__txt">Add to&nbsp;Cart</span>
+                            </div>
+                        </a>
+                        <img src="img/products/img1.png" alt="" class="card-product__image">
+                    </div>
+                    <div class="card-product__description">
+                        <a href="#" class="card-product__description__h4">${this.title}</a>
+                        <p class="card-product__description__p">${this.description}</p>
+                        <span class="card-product__price">${this.price}</span>
+                    </div>
+                </article> `;
+    }
+
 }
-renderCardList(prodCard);
+
+class ProductCardList {
+    constructor() {
+        this.cards = [];
+    }
+    fetchCards() {
+        this.cards = [
+            {
+                title: 'Shirt',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                price: 150
+            },
+            {
+                title: 'Socks',
+                description: 'Dolor sit amet, consectetur adipiscing elit',
+                price: 350
+            },
+            {
+                title: 'Shoes',
+                description: 'Consectetur adipiscing elit. Dolor sit amet, consectetur adipiscing elit',
+                price: 250
+            },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.cards.forEach(elem => {
+            const productCardItem = new ProductCardItem(elem.title, elem.price, elem.description);
+            listHtml += productCardItem.render();
+        })
+        document.querySelector('.filter').insertAdjacentHTML('afterend', listHtml);
+    }
+    calculate = () => {
+        let totalPrice = 0;
+        this.cards.forEach(elem => {
+            totalPrice += elem.price;
+        })
+        console.log(totalPrice);
+    }
+}
+
+let cardList = new ProductCardList();
+cardList.fetchCards();
+cardList.render();
+cardList.calculate();
